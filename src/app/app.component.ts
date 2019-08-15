860330b41806936254bf5963c2ea630575c56c85
tienanhcntt2 <VNW0013736@vn.fpg.com>
2019-08-15 08:26:03 +0700

    fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'SetTitleinAngular';
+  ngOnInit(): void {
+    throw new Error("Method not implemented.");
+  }
+
+  constructor(private serviceTitle: Title, private router:Router){
+
+  }
+  setTitleHome(){
+    this.serviceTitle.setTitle("Home");
+    this.router.navigateByUrl("/home");
+  }
+  setTitleLogin(){
+    this.serviceTitle.setTitle("Login" +" | " +this.title);
+    this.router.navigateByUrl("/login");
+  }
+  setTitleRegister(){
+    this.serviceTitle.setTitle("Register" +" | " +this.title);
+    this.router.navigateByUrl("/register");
+  }
 }
